2efc209dc2694529d5a358de69bc4e5d980ea747
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-05-27 19:44:25 +0200

    Add test to public interface

--- test/cartonik-test.js
+++ test/cartonik-test.js
@@ -3,108 +3,166 @@ import Cartonik from '../src'
 
 const EMPTY_POINT_MAP_XML = `<Map><Style name="points"><Rule><PointSymbolizer/></Rule></Style></Map>`
 
-describe('cartonik metatile = 1', function () {
-  beforeEach(function () {
-    this.cartonik = Cartonik.create()
+describe('cartonik', function () {
+  describe('metatile = 1', function () {
+    beforeEach(function () {
+      this.cartonik = Cartonik.create()
+    })
+
+    it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'wadus\' })', async function () {
+      const xml = EMPTY_POINT_MAP_XML
+
+      try {
+        await this.cartonik.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: 'wadus' })
+      } catch (error) {
+        assert.ok(error instanceof TypeError)
+        assert.equal(error.message, 'Format \'wadus\' not allowed')
+      }
+    })
+
+    describe('zoom = 0', function () {
+      it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 0, 0, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['0/0/0'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+    })
+
+    describe('zoom = 1', function () {
+      it('.tiles({ xml, coords: { z: 1, x: 0, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 0, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['1/0/0'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 1, x: 1, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 1, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['1/1/0'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 1, x: 0, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 0, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['1/0/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 1, x: 1, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 1, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['1/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+    })
   })
 
-  it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 0, 0, 0 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
+  describe('metatile = 4', function () {
+    beforeEach(function () {
+      this.cartonik = Cartonik.create({ metatile: 4 })
+    })
 
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
+    it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'wadus\' })', async function () {
+      const xml = EMPTY_POINT_MAP_XML
 
-    assert.deepEqual(Object.keys(tiles), ['0/0/0'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
+      try {
+        await this.cartonik.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: 'wadus' })
+      } catch (error) {
+        assert.ok(error instanceof TypeError)
+        assert.equal(error.message, 'Format \'wadus\' not allowed')
+      }
+    })
 
-  it('.tiles({ xml, coords: { z: 1, x: 1, y: 1 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 1, 1, 1 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
+    describe('zoom = 0', function () {
+      it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 0, 0, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
 
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
 
-    assert.deepEqual(Object.keys(tiles), ['1/1/1'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
+        assert.deepEqual(Object.keys(tiles), ['0/0/0'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+    })
 
-  it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'wadus\' })', async function () {
-    const xml = EMPTY_POINT_MAP_XML
+    describe('zoom = 1', function () {
+      it('.tiles({ xml, coords: { z: 1, x: 0, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 0, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
 
-    try {
-      await this.cartonik.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: 'wadus' })
-    } catch (error) {
-      assert.ok(error instanceof TypeError)
-      assert.equal(error.message, 'Format \'wadus\' not allowed')
-    }
-  })
-})
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
 
-describe('cartonik metatile = 4', function () {
-  beforeEach(function () {
-    this.cartonik = Cartonik.create({ metatile: 4 })
-  })
+        assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
 
-  it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 0, 0, 0 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
+      it('.tiles({ xml, coords: { z: 1, x: 1, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 1, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
 
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
 
-    assert.deepEqual(Object.keys(tiles), ['0/0/0'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
+        assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
 
-  it('.tiles({ xml, coords: { z: 1, x: 1, y: 0 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 1, 1, 0 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
+      it('.tiles({ xml, coords: { z: 1, x: 0, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 0, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
 
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
 
-    assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
+        assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
 
-  it('.tiles({ xml, coords: { z: 1, x: 0, y: 1 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 1, 0, 1 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
+      it('.tiles({ xml, coords: { z: 1, x: 1, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 1, 1, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
 
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
 
-    assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
-
-  it('.tiles({ xml, coords: { z: 1, x: 1, y: 1 }, format: \'png\' })', async function () {
-    const [ z, x, y ] = [ 1, 1, 1 ]
-    const coords = { z, x, y }
-    const xml = EMPTY_POINT_MAP_XML
-    const format = 'png'
-
-    const tiles = await this.cartonik.tiles({ xml, coords, format })
-
-    assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
-    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
-  })
-
-  it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'wadus\' })', async function () {
-    const xml = EMPTY_POINT_MAP_XML
-
-    try {
-      await this.cartonik.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: 'wadus' })
-    } catch (error) {
-      assert.ok(error instanceof TypeError)
-      assert.equal(error.message, 'Format \'wadus\' not allowed')
-    }
+        assert.deepEqual(Object.keys(tiles), ['1/0/0', '1/0/1', '1/1/0', '1/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+    })
   })
 })
