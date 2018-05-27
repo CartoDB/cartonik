09e73bd5ae5f7e4fd12610e9ba787b4ca4eadb1e
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-05-27 23:49:12 +0200

    Add test

--- test/cartonik-test.js
+++ test/cartonik-test.js
@@ -189,6 +189,150 @@ describe('cartonik', function () {
         assert.deepEqual(Object.keys(tiles), ['2/0/0', '2/0/1', '2/1/0', '2/1/1'])
         Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
       })
+
+      it('.tiles({ xml, coords: { z: 2, x: 0, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 0, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/0/0', '2/0/1', '2/1/0', '2/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 1, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 1, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/0/0', '2/0/1', '2/1/0', '2/1/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 2, y: 0 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 2, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/0', '2/2/1', '2/3/0', '2/3/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 2, y: 1 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 2, 1 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/0', '2/2/1', '2/3/0', '2/3/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 0, y: 2 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 0, 2 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/0/2', '2/0/3', '2/1/2', '2/1/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 1, y: 2 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 3, 0 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/0', '2/2/1', '2/3/0', '2/3/1'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 0, y: 3 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 0, 3 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/0/2', '2/0/3', '2/1/2', '2/1/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 1, y: 3 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 1, 3 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/0/2', '2/0/3', '2/1/2', '2/1/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 2, y: 2 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 2, 2 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/2', '2/2/3', '2/3/2', '2/3/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 3, y: 2 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 3, 2 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/2', '2/2/3', '2/3/2', '2/3/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 2, y: 3 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 2, 3 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/2', '2/2/3', '2/3/2', '2/3/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
+
+      it('.tiles({ xml, coords: { z: 2, x: 3, y: 3 }, format: \'png\' })', async function () {
+        const [ z, x, y ] = [ 2, 3, 3 ]
+        const coords = { z, x, y }
+        const xml = EMPTY_POINT_MAP_XML
+        const format = 'png'
+
+        const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+        assert.deepEqual(Object.keys(tiles), ['2/2/2', '2/2/3', '2/3/2', '2/3/3'])
+        Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+      })
     })
   })
 })
