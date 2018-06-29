e4b0923725fddab32e7b6c11efc1cf558d2c8afb
Daniel García Aubert <danielgarciaaubert@gmail.com>
2018-06-29 18:53:44 +0200

    Implement acceptance test that compares final tiles pixe by pixel

---
+++ test/support/match.js
@@ -0,0 +1,69 @@
+import fs from 'fs'
+import assert from 'assert'
+import path from 'path'
+import pixelmatch from 'pixelmatch'
+import { PNG } from 'pngjs'
+
+export default async function match (fixtureName, buffer, threshold = 0.005) {
+  const fixture = await getTileFixture(fixtureName)
+  // const tile = await getTile(body, fixtureName)
+
+  const tile = PNG.sync.read(buffer)
+  const diff = new PNG({
+    width: tile.width,
+    height: tile.height
+  })
+
+  const numDiffPixels = pixelmatch(tile.data, fixture.data, diff.data, tile.width, tile.height, { threshold })
+
+  await saveDiff(diff, fixtureName)
+  await saveResult(tile, fixtureName)
+
+  const difference = numDiffPixels / (fixture.width * fixture.height)
+
+  assert.ok(difference < threshold, `Tile ${fixtureName} does not match: difference(${difference}) < threshold(${threshold})`)
+}
+
+function getTileFixture (fixtureName) {
+  const fixturePath = path.join(process.cwd(), 'test/fixtures', `${fixtureName}.png`)
+
+  return new Promise((resolve, reject) => {
+    const fixture = fs.createReadStream(fixturePath)
+      .pipe(new PNG())
+      .on('parsed', () => resolve(fixture))
+      .on('error', err => reject(err))
+  })
+}
+
+// function getTile (body, fixtureName) {
+//   return new Promise((resolve, reject) => {
+//     const tile = body.pipe(new PNG())
+//       .on('parsed', () => resolve(tile))
+//       .on('error', err => reject(err))
+//   })
+// }
+
+function saveResult (tile, fixtureName) {
+  const buffer = PNG.sync.write(tile)
+  const resultPath = path.join(process.cwd(), 'test/results', `${fixtureName}.png`)
+
+  return new Promise((resolve, reject) => {
+    fs.writeFile(resultPath, buffer, err => err ? reject(err) : resolve())
+  })
+}
+
+function saveDiff (diff, fixtureName) {
+  const diffPath = path.join(process.cwd(), 'test/results', `${fixtureName}-diff.png`)
+  const diffStream = fs.createWriteStream(diffPath)
+
+  return new Promise((resolve, reject) => {
+    if (!diff.data) {
+      return resolve()
+    }
+
+    diff.pack()
+      .pipe(diffStream)
+      .on('error', err => reject(err))
+      .on('close', () => resolve())
+  })
+}
---
+++ test/world-map-test.js
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import path from 'path'
+import assert from 'assert'
+import mapnik from 'mapnik'
+import Cartonik from '../src'
+import match from './support/match'
+
+const WORLD_MAP_XML = fs.readFileSync('./test/fixtures/world-map.xml', { encoding: 'utf-8' })
+
+describe.only('world map', function () {
+  before(function () {
+    const shapeDatasource = path.join(mapnik.settings.paths.input_plugins, 'shape.input')
+    mapnik.register_datasource(shapeDatasource)
+  })
+
+  beforeEach(function () {
+    this.cartonik = Cartonik.create()
+  })
+
+  it('.tiles({ xml, coords: { z: 0, x: 0, y: 0 }, format: \'png\' })', async function () {
+    const xml = WORLD_MAP_XML
+    const [ z, x, y ] = [ 0, 0, 0 ]
+    const coords = { z, x, y }
+    const format = 'png'
+
+    const tiles = await this.cartonik.tiles({ xml, coords, format })
+
+    assert.deepEqual(Object.keys(tiles), ['0/0/0.png'])
+    Object.values(tiles).forEach(tile => assert.ok(tile instanceof Buffer))
+
+    await Promise.all(Object.values(tiles).map(tile => match('world-map-0.0.0', tile, 0.05)))
+
+    // Object.entries(tiles).forEach(tile => fs.writeFileSync(`world-map-${tile[0].split('/').join('.')}`, tile[1]))
+  })
+})
