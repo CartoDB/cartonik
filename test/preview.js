672dadbab3ff2164f13d7cafbe0cf8aa9371415c
Daniel García Aubert <danielgarciaaubert@gmail.com>
2019-06-11 19:21:08 +0200

    Cartofy test coming from abaculus

--- test/preview.js
+++ test/preview.js
@@ -1,9 +1,8 @@
 'use strict'
 
 const fs = require('fs')
-const path = require('path')
 const { promisify } = require('util')
-const { describe, it } = require('mocha')
+const { describe, it, before } = require('mocha')
 const assert = require('./support/assert')
 const rendererFactory = require('../lib/renderer-factory')
 const preview = require('../lib/preview')
@@ -13,7 +12,9 @@ const getTileList = require('../lib/preview/tiles')
 const getOffsetList = require('../lib/preview/offsets')
 const blend = require('../lib/preview/blend')
 
+const readFile = promisify(fs.readFile)
 const writeFile = promisify(fs.writeFile)
+const readDir = promisify(fs.readdir)
 
 // defaults
 const zoom = 5
@@ -26,30 +27,43 @@ const limit = 19008
 const tileSize = 256
 
 // fixtures
+async function getFixtureTiles () {
+  const tiles = {}
+  const filenames = await readDir('./test/fixtures/preview/tiles/')
 
-const tiles = fs.readdirSync(path.resolve(__dirname, 'fixtures/preview/tiles/')).reduce(function (memo, basename) {
-  const key = basename.split('.').slice(0, 4).join('.')
-  memo[key] = fs.readFileSync(path.resolve(__dirname, 'fixtures/preview/tiles/' + basename))
-  return memo
-}, {})
+  for (const filename of filenames) {
+    const key = filename.split('.').slice(0, 4).join('.')
+
+    tiles[key] = await readFile(`./test/fixtures/preview/tiles/${filename}`)
+  }
+
+  return tiles
+}
 
 describe('preview', function () {
-  describe('get center from bbox', function () {
+  let tiles
+
+  before(async function () {
+    tiles = await getFixtureTiles()
+  })
+
+  describe('dimensions', function () {
+    it('should should fail if the image is too large', function () {
+      const dimensions = { width: 4752, height: 4752 }
+      assert.throws(() => getDimensions({ dimensions, scale, limit }), /Desired image is too large./)
+    })
+
     it('should fail if (x1, y1) and (x2, y2) are equal', function () {
       const bbox = [0, 0, 0, 0]
-
-      assert.throws(function () {
-        getDimensions({ bbox, zoom, scale, tileSize, limit })
-      }, /Incorrect coordinates/)
+      assert.throws(() => getDimensions({ bbox, zoom, scale, tileSize, limit }), /Incorrect coordinates/)
     })
+
     it('should fail if the image is too large', function () {
       const bbox = [-60, -60, 60, 60]
       const zoom = 7
       const scale = 2
 
-      assert.throws(function () {
-        getDimensions({ bbox, zoom, scale, tileSize, limit })
-      }, /Desired image is too large./)
+      assert.throws(() => getDimensions({ bbox, zoom, scale, tileSize, limit }), /Desired image is too large./)
     })
 
     it('should return valid dimensions', function () {
@@ -61,29 +75,9 @@ describe('preview', function () {
       assert.deepStrictEqual(dimensions.width, 2730)
       assert.deepStrictEqual(dimensions.height, 3434)
     })
-
-    it('should return center in pixels from bbox', function () {
-      const bbox = [-60, -60, 60, 60]
-      const scale = 1
-
-      const center = getCenterInPixels({ bbox, zoom, scale, tileSize })
-
-      assert.deepStrictEqual(center.x, x)
-      assert.deepStrictEqual(center.y, y)
-    })
   })
 
-  describe('get coordinates from center', function () {
-    it('should should fail if the image is too large', function () {
-      const dimensions = {
-        width: 4752,
-        height: 4752
-      }
-      assert.throws(function () {
-        getDimensions({ dimensions, scale, limit })
-      }, /Desired image is too large./)
-    })
-
+  describe('center', function () {
     it('should return a valid center point in pixels', function () {
       const scale = 1
       let center = {
@@ -109,20 +103,37 @@ describe('preview', function () {
       assert.strictEqual(center.x, 623)
       assert.strictEqual(center.y, 552)
     })
+
+    it('should return center in pixels from bbox', function () {
+      const bbox = [-60, -60, 60, 60]
+      const scale = 1
+
+      const center = getCenterInPixels({ bbox, zoom, scale, tileSize })
+
+      assert.deepStrictEqual(center.x, x)
+      assert.deepStrictEqual(center.y, y)
+    })
   })
 
-  describe('create list of tile coordinates', function () {
+  describe('tiles', function () {
+    it('should fail when no tiles', async function () {
+      const center = {
+        width: 512,
+        height: 512
+      }
+      const dimensions = {
+        width: 0,
+        height: 0
+      }
+      assert.throws(() => getTileList({ zoom, scale, center, dimensions, tileSize }), /No coords object/)
+    })
+
     it('should return a valid coordinates object', function () {
       const zoom = 5
-
       const scale = 4
-
       const width = 1824
-
       const height = 1832
-
       const center = { x: 4096, y: 4096 }
-
       const dimensions = { width, height }
 
       const expectedCoords = [
@@ -131,45 +142,18 @@ describe('preview', function () {
         { z: zoom, x: 16, y: 15 },
         { z: zoom, x: 16, y: 16 }
       ]
+
       const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(coords), JSON.stringify(expectedCoords))
-    })
 
-    it('should return a valid offsets object', function () {
-      const zoom = 5
-
-      const scale = 4
-
-      const width = 1824
-
-      const height = 1832
-
-      const center = { x: 4096, y: 4096 }
-
-      const dimensions = { width, height }
-
-      const expectedOffsets = [
-        { x: -112, y: -108 },
-        { x: -112, y: 916 },
-        { x: 912, y: -108 },
-        { x: 912, y: 916 }
-      ]
-
-      const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(offsets), JSON.stringify(expectedOffsets))
+      assert.deepStrictEqual(coords, expectedCoords)
     })
 
     it('should return a valid coordinates object when image exceeds y coords', function () {
       const zoom = 2
-
       const scale = 1
-
       const width = 1000
-
       const height = 1000
-
       const center = { x: 623, y: 552 }
-
       const dimensions = { width, height }
 
       const expectedCoords = [
@@ -196,20 +180,112 @@ describe('preview', function () {
       ]
 
       const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(coords), JSON.stringify(expectedCoords))
+
+      assert.deepStrictEqual(coords, expectedCoords)
+    })
+
+    it('should return a valid coordinates object when image is much bigger than world', function () {
+      const zoom = 1
+      const scale = 1
+      const width = 2000
+      const height = 2100
+      const center = { x: 100, y: 100 }
+      const dimensions = { width, height }
+
+      const expectedCoords = [
+        { z: 1, x: -0, y: 0 },
+        { z: 1, x: -0, y: 1 },
+        { z: 1, x: 1, y: 0 },
+        { z: 1, x: 1, y: 1 },
+        { z: 1, x: -0, y: 0 },
+        { z: 1, x: -0, y: 1 },
+        { z: 1, x: 1, y: 0 },
+        { z: 1, x: 1, y: 1 },
+        { z: 1, x: 0, y: 0 },
+        { z: 1, x: 0, y: 1 },
+        { z: 1, x: 1, y: 0 },
+        { z: 1, x: 1, y: 1 },
+        { z: 1, x: 0, y: 0 },
+        { z: 1, x: 0, y: 1 },
+        { z: 1, x: 1, y: 0 },
+        { z: 1, x: 1, y: 1 },
+        { z: 1, x: 0, y: 0 },
+        { z: 1, x: 0, y: 1 }
+      ]
+
+      const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
+
+      assert.deepStrictEqual(coords, expectedCoords)
+    })
+
+    it('should return a valid coordinates object when image is smaller than world', function () {
+      const zoom = 1
+      const scale = 1
+      const width = 256
+      const height = 256
+      const center = { x: 256, y: 256 }
+      const dimensions = { width, height }
+
+      const expectedCoords = [
+        { z: 1, x: 0, y: 0 },
+        { z: 1, x: 0, y: 1 },
+        { z: 1, x: 1, y: 0 },
+        { z: 1, x: 1, y: 1 }
+      ]
+
+      const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
+
+      assert.deepStrictEqual(coords, expectedCoords)
+    })
+
+    it('should return a valid offset object when image is smaller than world', function () {
+      const zoom = 1
+      const scale = 1
+      const width = 256
+      const height = 256
+      const center = { x: 256, y: 256 }
+      const dimensions = { width, height }
+
+      const expectedOffset = [
+        { x: -128, y: -128 },
+        { x: -128, y: 128 },
+        { x: 128, y: -128 },
+        { x: 128, y: 128 }
+      ]
+
+      const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
+
+      assert.deepStrictEqual(offsets, expectedOffset)
+    })
+  })
+
+  describe('offsets', function () {
+    it('should return a valid offsets object', function () {
+      const zoom = 5
+      const scale = 4
+      const width = 1824
+      const height = 1832
+      const center = { x: 4096, y: 4096 }
+      const dimensions = { width, height }
+
+      const expectedOffsets = [
+        { x: -112, y: -108 },
+        { x: -112, y: 916 },
+        { x: 912, y: -108 },
+        { x: 912, y: 916 }
+      ]
+
+      const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
+
+      assert.deepStrictEqual(offsets, expectedOffsets)
     })
 
     it('should return a valid offsets object when image exceeds y coords', function () {
       const zoom = 2
-
       const scale = 1
-
       const width = 1000
-
       const height = 1000
-
       const center = { x: 623, y: 552 }
-
       const dimensions = { width, height }
 
       const expectedOffsets = [
@@ -236,58 +312,16 @@ describe('preview', function () {
       ]
 
       const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(offsets), JSON.stringify(expectedOffsets))
-    })
 
-    it('should return a valid coordinates object when image is much bigger than world', function () {
-      const zoom = 1
-
-      const scale = 1
-
-      const width = 2000
-
-      const height = 2100
-
-      const center = { x: 100, y: 100 }
-
-      const dimensions = { width, height }
-
-      const expectedCoords = [
-        { z: zoom, x: 0, y: 0 },
-        { z: zoom, x: 0, y: 1 },
-        { z: zoom, x: 1, y: 0 },
-        { z: zoom, x: 1, y: 1 },
-        { z: zoom, x: 0, y: 0 },
-        { z: zoom, x: 0, y: 1 },
-        { z: zoom, x: 1, y: 0 },
-        { z: zoom, x: 1, y: 1 },
-        { z: zoom, x: 0, y: 0 },
-        { z: zoom, x: 0, y: 1 },
-        { z: zoom, x: 1, y: 0 },
-        { z: zoom, x: 1, y: 1 },
-        { z: zoom, x: 0, y: 0 },
-        { z: zoom, x: 0, y: 1 },
-        { z: zoom, x: 1, y: 0 },
-        { z: zoom, x: 1, y: 1 },
-        { z: zoom, x: 0, y: 0 },
-        { z: zoom, x: 0, y: 1 }
-      ]
-
-      const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(coords), JSON.stringify(expectedCoords))
+      assert.deepStrictEqual(offsets, expectedOffsets)
     })
 
     it('should return a valid offsets object when image is much bigger than world', function () {
       const zoom = 1
-
       const scale = 1
-
       const width = 2000
-
       const height = 2100
-
       const center = { x: 100, y: 100 }
-
       const dimensions = { width, height }
 
       const expectedOffsets = [
@@ -312,62 +346,23 @@ describe('preview', function () {
       ]
 
       const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
-      assert.deepStrictEqual(JSON.stringify(offsets), JSON.stringify(expectedOffsets))
+
+      assert.deepStrictEqual(offsets, expectedOffsets)
     })
+  })
 
-    it('should return a valid coordinates object when image is smaller than world', function () {
-      const zoom = 1
+  describe('blend', function () {
+    const sizes = [256, 512, 1024]
 
-      const scale = 1
+    sizes.forEach((size) => {
+      function getTileTest (format, z, x, y) {
+        const key = [z, x, y, size].join('.')
 
-      const width = 256
+        return !tiles[key]
+          ? Promise.reject(new Error('Tile does not exist'))
+          : Promise.resolve({ tile: tiles[key] })
+      }
 
-      const height = 256
-
-      const center = { x: 256, y: 256 }
-
-      const dimensions = { width, height }
-
-      const expectedCoords = [
-        { z: 1, x: 0, y: 0 },
-        { z: 1, x: 0, y: 1 },
-        { z: 1, x: 1, y: 0 },
-        { z: 1, x: 1, y: 1 }
-      ]
-
-      const coords = getTileList({ zoom, scale, center, dimensions, tileSize })
-
-      assert.deepStrictEqual(JSON.stringify(coords), JSON.stringify(expectedCoords))
-    })
-
-    it('should return a valid offset object when image is smaller than world', function () {
-      const zoom = 1
-
-      const scale = 1
-
-      const width = 256
-
-      const height = 256
-
-      const center = { x: 256, y: 256 }
-
-      const dimensions = { width, height }
-
-      const expectedOffset = [
-        { x: -128, y: -128 },
-        { x: -128, y: 128 },
-        { x: 128, y: -128 },
-        { x: 128, y: 128 }
-      ]
-
-      const offsets = getOffsetList({ zoom, scale, center, dimensions, tileSize })
-
-      assert.deepStrictEqual(JSON.stringify(offsets), JSON.stringify(expectedOffset))
-    })
-  });
-
-  [256, 512, 1024].forEach(function (size) {
-    describe('stitch tiles into single png', function () {
       const coords = [
         { z: 1, x: 0, y: 0 },
         { z: 1, x: 0, y: 1 },
@@ -387,31 +382,29 @@ describe('preview', function () {
         height: size * 2
       }
 
-      it('should fail when no tiles', async function () {
-        const dimensions = {
-          width: 0,
-          height: 0
-        }
-
-        try {
-          getTileList({ zoom, scale, center, dimensions, tileSize })
-          throw new Error('Should not throw')
-        } catch (err) {
-          assert.strictEqual(err.message, 'No coords object')
-        };
-      })
-
-      it('should return tiles and stitch them together', async function () {
+      it(`should stitch tiles together using size = ${size}`, async function () {
         const { image } = await blend({ coordinates: coords, offsets, dimensions: center, format, quality, getTile: getTileTest })
 
-        await writeFile(path.resolve(__dirname, 'fixtures/output/pngs/preview-expected.' + size + '.png'), image)
+        await writeFile('./test/fixtures/output/pngs/preview-expected.' + size + '.png', image)
 
         await assert.imageEqualsFile(image, `./test/fixtures/preview/expected/expected.${size}.png`)
       })
     })
+  })
 
-    describe('run entire function', function () {
-      it('stitches images with a center coordinate', async function () {
+  describe('preview', function () {
+    const sizes = [256, 512, 1024]
+
+    sizes.forEach(function (size) {
+      function getTileTest (format, z, x, y) {
+        const key = [z, x, y, size].join('.')
+
+        return !tiles[key]
+          ? Promise.reject(new Error('Tile does not exist'))
+          : Promise.resolve({ tile: tiles[key] })
+      }
+
+      it('should stitch images using center coordinate', async function () {
         const params = {
           zoom: 1,
           scale: 1,
@@ -431,18 +424,16 @@ describe('preview', function () {
 
         const { image } = await preview(params)
 
-        await writeFile(path.resolve(__dirname, 'fixtures/output/pngs/preview-center.' + size + '.png'), image)
-
-        console.log('\tVisually check image at ' + path.resolve(__dirname, 'fixtures/output/pngs/preview-center.' + size + '.png'))
+        await writeFile('./test/fixtures/output/pngs/preview-center.' + size + '.png', image)
 
         await assert.imageEqualsFile(image, `./test/fixtures/preview/expected/center.${size}.png`)
       })
 
-      it('stitches images with a wsen bbox', async function () {
+      it('should stitch images using a bounding box (wsen)', async function () {
         const params = {
           zoom: 1,
           scale: 1,
-          bbox: [-140, -80, 140, 80],
+          bbox: [ -140, -80, 140, 80 ],
           format: 'png',
           quality: 50,
           tileSize: size,
@@ -451,24 +442,12 @@ describe('preview', function () {
 
         const { image } = await preview(params)
 
-        writeFile(path.resolve(__dirname, 'fixtures/output/pngs/preview-bbox.' + size + '.png'), image)
-
-        console.log('\tVisually check image at ' + path.resolve(__dirname, 'fixtures/output/pngs/preview-bbox.' + size + '.png'))
+        writeFile('./test/fixtures/output/pngs/preview-bbox.' + size + '.png', image)
 
         await assert.imageEqualsFile(image, `./test/fixtures/preview/expected/bbox.${size}.png`)
       })
     })
 
-    function getTileTest (format, z, x, y) {
-      const key = [z, x, y, size].join('.')
-
-      return !tiles[key]
-        ? Promise.reject(new Error('Tile does not exist'))
-        : Promise.resolve({ tile: tiles[key] })
-    }
-  })
-
-  describe('acceptance', function () {
     const rendererOptions = {
       type: 'raster',
       xml: fs.readFileSync('./test/fixtures/mmls/world-borders.xml', 'utf8'),
@@ -479,7 +458,7 @@ describe('preview', function () {
       xml: fs.readFileSync('./test/fixtures/mmls/world-borders-interactivity.xml', 'utf8')
     })
 
-    it('stitches images with bbox', async function () {
+    it('should use cartonik\'s getTile method to fetch tiles and stich them using bbox', async function () {
       const renderer = rendererFactory(options)
 
       const params = {
@@ -500,7 +479,7 @@ describe('preview', function () {
       await renderer.close()
     })
 
-    it('stitches images with center', async function () {
+    it('should use cartonik\'s getTile method to fetch tiles and stich them using center', async function () {
       const renderer = rendererFactory(options)
 
       const params = {
